f4308d999a57af86f1cd3fce0ab2ce8831436118
shashank00818 <shashank00818@gmail.com>
2023-04-30 00:43:20 +0530

    lec 4 commit

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    GENDER = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Others")
+    ]
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    age = models.IntegerField(default=0, null=True)
+    email = models.EmailField()
+    gender = models.CharField(max_length=1, choices=GENDER)
