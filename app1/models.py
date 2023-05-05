2e715b7ba0ea19641af9bcae34bb2aaac24c1be0
shashank00818 <shashank00818@gmail.com>
2023-05-05 16:07:16 +0530

    remove colum from table

--- app1/models.py
+++ app1/models.py
@@ -11,4 +11,4 @@ class Person(models.Model):
     lname = models.CharField(max_length=50)
     age = models.IntegerField(default=0, null=True)
     email = models.EmailField()
-    gender = models.CharField(max_length=1, choices=GENDER)
+    # gender = models.CharField(max_length=1, choices=GENDER)
