ed55adefbd5101056bb8cd3a0fbe6f0f98d77330
shashank00818 <shashank00818@gmail.com>
2023-05-05 16:45:08 +0530

    enum changes

--- app1/models.py
+++ app1/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
 # Create your models here.
 class Person(models.Model):
-    GENDER = [
-        ("M", "Male"),
-        ("F", "Female"),
-        ("O", "Others")
-    ]
+
+    GENDER = Choices(
+        (1, "male", _("male")),
+        (2, "female", _("female")),
+        (3, "others", _("others")),
+    )
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     age = models.IntegerField(default=0, null=True)
     email = models.EmailField()
-    # gender = models.CharField(max_length=1, choices=GENDER)
+    gender = models.CharField(max_length=1, choices=GENDER, default=GENDER.male)
